24c79a20ef8fee7d99038d522a2570ed6dffbd52
Krz <jrrobertokrz@gmail.com>
2024-04-17 16:57:32 -0300

    Resolvendo exercício 01

---
+++ 2024-1/INTRODUÇÃO À PROGRAMAÇÃO ESTRUTURADA EM C/aula-01-loops/tabuada/tabuada.c
@@ -0,0 +1,24 @@
+/*
+
+Professor: Eliton Marcos Vela
+
+Problemática: Fazer um programa em C que imprime uma tabela com a tabuada de 1 a 9
+
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    for (int numero = 1; numero <= 9; numero++)
+    {
+        printf("Tabuada do %d\n", numero);
+        for (int multiplicador = 1; multiplicador <= 10; multiplicador++)
+        {
+            int resultado = numero * multiplicador;
+            printf("%d x %d = %d\n", numero, multiplicador, resultado);
+        }
+        printf("\n");
+    }
+    return 0;
+}
