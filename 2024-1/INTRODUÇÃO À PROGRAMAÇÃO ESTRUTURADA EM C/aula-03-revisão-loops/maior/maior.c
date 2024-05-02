410dad9996dccdfe4c6ec41d9dfcc08b51341e3f
Krz <jrrobertokrz@gmail.com>
2024-05-02 14:47:31 -0300

    Resolvendo exercício 02

---
+++ 2024-1/INTRODUÇÃO À PROGRAMAÇÃO ESTRUTURADA EM C/aula-03-revisão-loops/maior/maior.c
@@ -0,0 +1,28 @@
+/*
+
+Professor: Eliton Marcos Vela
+
+Problemática: Desenvolva um programa que
+leia 15 números e mostre o maior deles.
+
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int maiorNumero = 0, numero = 0;
+
+    for (int i = 0; i <= 15; i++)
+    {
+        /* Solicita entrada ao usuário */
+        printf("Digite um número positivo e inteiro: ");
+        scanf("%d", &numero);
+        if (numero > maiorNumero) {
+            maiorNumero = numero;
+        }
+    }
+    printf("O maior número recebido foi: %d", maiorNumero);
+
+    return 0;
+}
