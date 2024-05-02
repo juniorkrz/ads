f135bf222b3f569613adab7d1a798f496b89a617
Krz <jrrobertokrz@gmail.com>
2024-05-02 14:47:15 -0300

    Resolvendo exercício 01

---
+++ 2024-1/INTRODUÇÃO À PROGRAMAÇÃO ESTRUTURADA EM C/aula-03-revisão-loops/for/for.c
@@ -0,0 +1,27 @@
+/*
+
+Professor: Eliton Marcos Vela
+
+Problemática: Desenvolva um programa que
+leia um número e o mostre 20 vezes.
+
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int num;
+
+    /* Solicita entrada ao usuário */
+    printf("Digite um número: ");
+    scanf("%d", &num);
+
+    /* Exibe o número 20 vezes */
+    for (int i = 0; i <= 20; i++)
+    {
+        printf("%d\n", num);
+    }
+
+    return 0;
+}
